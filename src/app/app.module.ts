df1e430c6e4dd6e102320b2bd7af31e7327699f4
Ricardo Gaitan <ricardogaitan>
2019-10-18 10:24:57 -0500

    <Arquitectura Angular> Se crean los componentes Home y About

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
